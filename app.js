1c0183bfb2349a5bd05c3a664c0a38c14f92071c
fierodev <andriybigyn31@gmail.com>
2021-12-14 22:00:49 +0200

    added DB

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const express = require("express");
 // import Routes
 const RegistrationRouter = require("./routes/usersRouter");
 
+// Import Database
+const db = require("./data/config/database");
+db.authenticate()
+.then(() => console.log("Database conected... "))
+.catch(err => console.log("Connection error => ", err));
 
 const PORT = 5000;
 const app = express();
 
 app.use((req,res, next) => {
     console.log("Request => ", new Date());
-    res.header("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-
-"Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token"
-
-);
-
-res.header("Access-Control-Allow-Methods","DELETE, POST, PUT, GET");
     next();
 });
 
